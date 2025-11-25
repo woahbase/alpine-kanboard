06fb934a909dd4bc681c79923ada236e47e6ab13
lapnapra <34398784+lapnapra@users.noreply.github.com>
2025-11-25 13:21:55 +0530

    update README, makefile, fix permission issues when run as non-root user

--- root/defaults/cronjob.sh
+++ root/defaults/cronjob.sh
@@ -3,6 +3,8 @@ set -e
 
 # 0 8 * * * cd /var/www/app && php83 ./cli cronjob >/dev/null 2>&1
 
+usercmd () { if [ "X${EUID}" != "X0" ]; then ${1} "${@:2}"; else s6-setuidgid ${PUID:-1000}:${PGID:-1000} ${1} "${@:2}"; fi; }
+
 ROOTDIR="${ROOTDIR:-/config}";
 WEBDIR="${WEBDIR:-$ROOTDIR/www}";
 KANBOARDDIR="${KANBOARDDIR:-$WEBDIR/kanboard}"; # note: no ending /
@@ -11,6 +13,6 @@ cd ${KANBOARDDIR} || exit 1;
 
 echo "[$(date -R)] Running Kanboard CRON Job...";
 
-s6-setuidgid ${S6_USER:-alpine} \
+usercmd \
     php ./cli cronjob \
     # >/dev/stdout 2>/dev/stderr
